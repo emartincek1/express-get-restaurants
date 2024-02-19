2ef2f6e6c04a2133958d549ce6af6fe483260702
emartincek1 <evan.martincek@gmail.com>
2024-02-19 11:29:52 -0600

    add crud to restaurants with error handling

--- src/app.js
+++ src/app.js
@@ -3,10 +3,64 @@ const app = express();
 const Restaurant = require("../models/index");
 const db = require("../db/connection");
 
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
 //TODO: Create your GET Request Route Below:
-app.get("/restaurants", async (req, res) => {
-  const restaurants = await Restaurant.findAll();
-  res.json(restaurants);
+app.get("/restaurants", async (req, res, next) => {
+  try {
+    const restaurants = await Restaurant.findAll();
+    res.json(restaurants);
+  } catch (e) {
+    next(e);
+  }
+});
+
+app.get("/restaurants/:id", async (req, res, next) => {
+  try {
+    const { id } = req.params;
+    const restuarant = await Restaurant.findByPk(id);
+    if (restuarant === null) return res.send(`No restaurant at ${id}.`);
+    res.json(restuarant);
+  } catch (e) {
+    next(e);
+  }
+});
+
+app.post("/restaurants", async (req, res, next) => {
+  try {
+    const { restaurant } = req.body;
+    await Restaurant.create(restaurant);
+    const restaurants = await Restaurant.findAll();
+    res.json(restaurants);
+  } catch (e) {
+    next(e);
+  }
+});
+
+app.put("/restaurants/:id", async (req, res, next) => {
+  try {
+    const { restaurant } = req.body;
+    const { id } = req.params;
+    const oldRestaurant = await Restaurant.findByPk(id);
+    await oldRestaurant.update(restaurant);
+    const restaurants = await Restaurant.findAll();
+    res.json(restaurants);
+  } catch (e) {
+    next(e);
+  }
+});
+
+app.delete("/restaurants/:id", async (req, res, next) => {
+  try {
+    const { id } = req.params;
+    const oldRestaurant = await Restaurant.findByPk(id);
+    await oldRestaurant.destroy();
+    const restaurants = await Restaurant.findAll();
+    res.json(restaurants);
+  } catch (e) {
+    next(e);
+  }
 });
 
 module.exports = app;
