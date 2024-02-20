be2a4bf3cf0fa9cff55e80de3b74301a42860a6f
emartincek1 <evan.martincek@gmail.com>
2024-02-20 12:15:45 -0600

    add unit tests

---
+++ index.test.js
@@ -0,0 +1,75 @@
+const request = require("supertest");
+const app = require("./src/app");
+const Restaurant = require("./models/index");
+const syncSeed = require("./seed");
+
+describe("Server Tests", () => {
+  beforeAll(async () => {
+    await syncSeed();
+  });
+
+  test("get restaurants returns correct response", async () => {
+    const response = await request(app).get("/restaurants");
+    expect(response.statusCode).toBe(200);
+    expect(response.body[0]).toEqual(
+      expect.objectContaining({
+        name: "AppleBees",
+        location: "Texas",
+      })
+    );
+    const restaurants = await Restaurant.findAll();
+    expect(response.body.length).toBe(restaurants.length);
+  });
+
+  test("post restaurants returns correct response", async () => {
+    const response = await request(app)
+      .post("/restaurants")
+      .send({
+        restaurant: {
+          name: "ChikFilA",
+          location: "Atlanta",
+          cuisine: "FastFood",
+        },
+      });
+    expect(response.statusCode).toBe(200);
+    expect(response.body[3]).toEqual(
+      expect.objectContaining({
+        name: "ChikFilA",
+        location: "Atlanta",
+        cuisine: "FastFood",
+      })
+    );
+    const restaurants = await Restaurant.findAll();
+    expect(response.body.length).toBe(restaurants.length);
+  });
+
+  test("put restaurants returns correct response", async () => {
+    const response = await request(app)
+      .put("/restaurants/4")
+      .send({
+        restaurant: {
+          name: "FiveGuys",
+          location: "Atlanta",
+          cuisine: "FastFood",
+        },
+      });
+    expect(response.statusCode).toBe(200);
+    expect(response.body[3]).toEqual(
+      expect.objectContaining({
+        name: "FiveGuys",
+        location: "Atlanta",
+        cuisine: "FastFood",
+      })
+    );
+    const restaurants = await Restaurant.findAll();
+    expect(response.body.length).toBe(restaurants.length);
+  });
+
+  test("delete restaurants returns correct response", async () => {
+    const response = await request(app).delete("/restaurants/4");
+    expect(response.statusCode).toBe(200);
+    expect(response.body[3]).toEqual(undefined);
+    const restaurants = await Restaurant.findAll();
+    expect(response.body.length).toBe(restaurants.length);
+  });
+});
